44606d888bf4f1cc2a474822a63c61c293c4d6c3
Valerio Persiani <176795308+ValerioP29@users.noreply.github.com>
2026-02-19 11:51:52 +0100

    Fix reminder command registration without Artisan::starting

--- routes/console.php
+++ routes/console.php
@@ -1,15 +1,8 @@
 <?php
 
-use App\Console\Commands\DispatchDueRemindersCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-Artisan::starting(function ($artisan): void {
-    $artisan->resolveCommands([
-        DispatchDueRemindersCommand::class,
-    ]);
-});
